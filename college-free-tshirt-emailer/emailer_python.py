5075db8d4ddaf3f2214bd33ac44d9b7f03340922
ColeFeely6 <65377055+ColeFeely6@users.noreply.github.com>
2023-02-17 17:34:49 -0500

    Update emailer_python.py

--- college-free-tshirt-emailer/emailer_python.py
+++ college-free-tshirt-emailer/emailer_python.py
@@ -14,15 +14,7 @@ with smtplib.SMTP('smtp.gmail.com',587) as smtp:
     smtp.login(my_email,my_password)
     
 
-    '''subject = 'Considering attending in the spring'
-    body = 'Dear Office of Admissions, \n\n Due to the recent pandemic, my family must hold off from sending me to college this fall. I was considering attending %s in the spring for the beautiful campus and great student life! I would still love to show my support for the school but I have not gotten the chance to visit campus and buy apparel. I would love it if you could send me T- shirt if you could! I am a size medium and my address is: \n Cole Feely \n 393 Lebanon Street \n Melrose, MA 02176 \n Thank you so much and hopefully I will be on campus in the spring, \n Cole Feely'%'UMass'
-    
-    
-    
-    msg = f'Subject: {subject}\n\n{body}'
-    smtp.sendmail(my_email,'colehfeely@gmail.com',msg)
-    #for i in range(len(sp.names)):
-     #   smtp.sendmail(my_email,str(sp.emails[i]),msg)'''
+
     
     
     
@@ -30,6 +22,6 @@ with smtplib.SMTP('smtp.gmail.com',587) as smtp:
         print(sp.names[i],i)
         print(sp.emails[i])
         subject = 'Consider attending in the spring'
-        body = 'Dear Office of Admissions, \n\n Due to the recent pandemic, my family must hold off from sending me to college this fall. I was considering attending %s in the spring for the beautiful campus and great student life! I would still love to show my support for the school but I have not gotten the chance to visit campus and buy apparel. I would love it if you could send me T- shirt if you could! I am a size medium and my address is: \n Cole Feely \n 393 Lebanon Street \n Melrose, MA 02176 \n Thank you so much and hopefully I will be on campus in the spring, \n Cole Feely'%sp.names[i]
+        body = 'Dear Office of Admissions, (Message asking for T-Shirt) (Address) (Sincerely, Name)'%sp.names[i]
         msg = f'Subject: {subject}\n\n{body}'
         smtp.sendmail(my_email,sp.emails[i],msg)
